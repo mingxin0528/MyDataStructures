de66e1797b4a19b370fe68e74dc9e6cef597e528
Ming <1003217688@qq.com>
2022-12-23 15:10:40 +0000

    Some Basic Data Structures

---
+++ MyDataStructures/src/MyArrayQueue.java
@@ -0,0 +1,24 @@
+public class MyArrayQueue<E> {
+    private MyArrayList<E> data;
+    public MyArrayQueue(){
+        data = new MyArrayList<>();
+    }
+
+//    public void offer(E element){
+//        data.add(element);
+//    }
+//    public E poll(){
+//        return data.removeFirst();
+//    }
+//    public E peek(){
+//        return data.getFirst();
+//    }
+
+//    public static void main(String[] args) {
+//        MyLinkedQueue<Integer> q = new MyLinkedQueue<>();
+//        q.offer(1);
+//        q.offer(2);
+//        System.out.println(q.poll());
+//    }
+}
+
---
+++ MyDataStructures/src/MyLinkedQueue.java
@@ -0,0 +1,23 @@
+public class MyLinkedQueue<E> {
+    private MyDoublyLinkedList<E> data;
+    public MyLinkedQueue(){
+        data = new MyDoublyLinkedList<>();
+    }
+
+    public void offer(E element){
+        data.addLast(element);
+    }
+    public E poll(){
+        return data.removeFirst();
+    }
+    public E peek(){
+        return data.getFirst();
+    }
+
+//    public static void main(String[] args) {
+//        MyLinkedQueue<Integer> q = new MyLinkedQueue<>();
+//        q.offer(1);
+//        q.offer(2);
+//        System.out.println(q.poll());
+//    }
+}
